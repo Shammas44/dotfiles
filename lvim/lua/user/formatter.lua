3ef84fc5cee3a3c771afbce1be4974021460a213
Shammas44 <sebastien.traber@hotmail.com>
2023-04-14 17:59:01 +0200

    auto update 2023-04-14 17:59:01

--- lvim/lua/user/formatter.lua
+++ lvim/lua/user/formatter.lua
@@ -3,5 +3,6 @@ formatters.setup {
     -- { command = "black", filetypes = { "python" } },
     -- { command = "isort", filetypes = { "python" } },
     { command = "prettier",     extra_args = { "--print-with", "100" }, filetypes = { "typescript", "typescriptreact", "css", "scss" } },
-    { command = "markdownlint", extra_args = {}, filetypes = { "markdown", "vimwiki" } }
+    { command = "markdownlint", extra_args = {}, filetypes = { "markdown", "vimwiki" } },
+    { command = "yamlfmt", extra_args = {}, filetypes = { "yml", "yaml" } }
 }
