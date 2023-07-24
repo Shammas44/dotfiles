ba80682f313c650fa3ecce7dec8f286e80d1a6be
Shammas44 <sebastien.traber@hotmail.com>
2023-07-24 17:59:00 +0200

    auto update 2023-07-24 17:59:00

--- lvim/lua/user/lsp.lua
+++ lvim/lua/user/lsp.lua
@@ -46,6 +46,9 @@
 --     }
 -- })
 -- require("lvim.lsp.manager").setup("clangd", {
---     cmd = { "clangd", "--background-index" },
---     filetypes = { "c", "cpp", "objc", "objcpp", "ino" },
+--   cmd = { "clangd", "--background-index" },
+--   filetypes = { "c", "cpp", "objc", "objcpp", "ino" },
 -- })
+-- local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities.offsetEncoding = { "utf-16" }
+-- require("lspconfig").clangd.setup({ capabilities = capabilities })
