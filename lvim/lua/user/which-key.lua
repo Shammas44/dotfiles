b12e05d3a5aacfebd51f21f2a75c204c8009d4ad
Shammas44 <sebastien.traber@hotmail.com>
2023-08-12 17:59:00 +0200

    auto update 2023-08-12 17:59:00

--- lvim/lua/user/which-key.lua
+++ lvim/lua/user/which-key.lua
@@ -1,13 +1,16 @@
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
 lvim.builtin.which_key.mappings["t"] = {
-  name = "+Trouble",
-  r = { "<cmd>Trouble lsp_references<cr>", "References" },
-  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
-  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
-  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
-  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
-  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Workspace Diagnostics" },
+  name = "+QuickfixList",
+  o = { "<cmd>copen<cr>", "open" },
+  l = { "<cmd>cnext<cr>", "next" },
+  h = { "<cmd>cprev<cr>", "previous" },
+}
+lvim.builtin.which_key.mappings["T"] = {
+  name = "+LocationList",
+  o = { "<cmd>lopen<cr>", "open" },
+  l = { "<cmd>lnext<cr>", "next" },
+  h = { "<cmd>lprev<cr>", "previous" },
 }
 lvim.builtin.which_key.mappings["m"] = {
   name = "+Session Management",
