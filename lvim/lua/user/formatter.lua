aab89e414b9ae294ff85312693ae87fb23d07e26
Shammas44 <sebastien.traber@hotmail.com>
2023-09-05 17:59:01 +0200

    auto update 2023-09-05 17:59:00

--- lvim/lua/user/formatter.lua
+++ lvim/lua/user/formatter.lua
@@ -1,6 +1,6 @@
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
-    -- { command = "black", filetypes = { "python" } },
+    { command = "black", filetypes = { "python" } },
     -- { command = "isort", filetypes = { "python" } },
     { command = "prettier",     extra_args = { "--print-with", "100" }, filetypes = { "typescript", "typescriptreact", "css", "scss" } },
     { command = "markdownlint", extra_args = {}, filetypes = { "markdown", "vimwiki" } },
