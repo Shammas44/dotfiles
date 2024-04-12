8ad4670261a49499a3e591f8fdf69d79469a8531
Shammas44 <sebastien.traber@hotmail.com>
2024-04-12 17:59:00 +0200

    auto update 2024-04-12 17:59:00

--- lvim/lua/user/debbugers.lua
+++ lvim/lua/user/debbugers.lua
@@ -68,37 +68,37 @@ dap.configurations.javascript = {
   },
 }
 
--- dap.configurations.cpp = {
---   {
---     name = "Launch file",
---     type = "cppdbg",
---     request = "launch",
---     program = function()
---       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
---     end,
---     cwd = '${workspaceFolder}',
---     miDebuggerPath = '/usr/local/bin/gdb',
---     targetArchitecture = 'x86_64',
---     stopAtEntry = true,
---     filterStdout = true,
---     externalConsole = true,
---   },
---   {
---     name = 'Attach to gdbserver :1234',
---     type = 'cppdbg',
---     request = 'launch',
---     MIMode = 'gdb',
---     miDebuggerServerAddress = 'localhost:1234',
---     miDebuggerPath = '/usr/local/bin/gdb',
---     externalConsole = true,
---     filterStdout = true,
---     cwd = '${workspaceFolder}',
---     targetArchitecture = 'x86_64',
---     program = function()
---       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
---     end,
---   },
--- }
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    miDebuggerPath = '/usr/local/bin/gdb',
+    targetArchitecture = 'x86_64',
+    stopAtEntry = true,
+    filterStdout = true,
+    externalConsole = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/local/bin/gdb',
+    externalConsole = true,
+    filterStdout = true,
+    cwd = '${workspaceFolder}',
+    targetArchitecture = 'x86_64',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
 -- dap.configurations.c = dap.configurations.cpp
 
 dap.configurations.c = {
@@ -113,5 +113,3 @@ dap.configurations.c = {
     stopAtBeginningOfMainSubprogram = false,
   },
 }
-
--- require("user.dap.javascript").setup()
