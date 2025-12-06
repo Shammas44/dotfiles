2a8a358c05682cdc8a25c845f6a078a73f80737a
Shammas44 <sebastien.traber@hotmail.com>
2025-12-06 17:59:02 +0100

    auto update 2025-12-06 17:59:00

--- .config/lvim/lua/user/remapping.lua
+++ .config/lvim/lua/user/remapping.lua
@@ -21,6 +21,15 @@ lvim.keys.normal_mode["<Leader>W"] = ":!~/dotfiles/vim/.vim/getIndex.ps1<cr> | <
 
 -- For switch to last edited buffer
 vim.keymap.set('n', '<C-n>', '<C-6>')
+vim.keymap.set('n', '<C-w>1', '1<C-w><C-w>')
+vim.keymap.set('n', '<C-w>2', '2<C-w><C-w>')
+vim.keymap.set('n', '<C-w>3', '3<C-w><C-w>')
+vim.keymap.set('n', '<C-w>4', '4<C-w><C-w>')
+vim.keymap.set('n', '<C-w>5', '5<C-w><C-w>')
+vim.keymap.set('n', '<C-w>6', '6<C-w><C-w>')
+vim.keymap.set('n', '<C-w>7', '7<C-w><C-w>')
+vim.keymap.set('n', '<C-w>8', '8<C-w><C-w>')
+vim.keymap.set('n', '<C-w>9', '9<C-w><C-w>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>')
 vim.keymap.set('i', 'jk', '<Esc>')
 vim.keymap.set('c', 'jk', '<Esc>')
